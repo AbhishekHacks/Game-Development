478addee580e2acd730d0b779a39b61b9a836258
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-03-15 02:44:28 +0530

    Updated main.cpp

--- SAVE DORA/main.cpp
+++ SAVE DORA/main.cpp
@@ -19,16 +19,47 @@ spriteBackground.setPosition(0,0);
 
 //Fish
 Texture textureFish;
-textureFish.loadFromFile("graphics1/fish-picaai-Photoroom.png");
+textureFish.loadFromFile("coverphoto.png");
 Sprite spriteFish;
 spriteFish.setTexture(textureFish);
-FloatRect bounds = spriteFish.getLocalBounds();
-float width = bounds.width;
-spriteFish.setPosition(0,2160/2-350);
-spriteFish.setScale(0.8f,0.8f);
+spriteFish.setPosition(2892,680);
+spriteFish.setScale(2.5f,2.5f);
+spriteFish.setOrigin(spriteFish.getLocalBounds().width/2,spriteFish.getLocalBounds().height/2);
 
 
+//font
+Font font1;
+font1.loadFromFile("font/KOMIKAP_.ttf");
+Text text1;
+text1.setFont(font1);
+text1.setString("Press Enter To Play");
+text1.setCharacterSize(100);
+text1.setFillColor(Color::White);
+text1.setPosition(3856/4-150,1500);
+text1.setScale(2.0f,2.0f);
+
+
+float fishSpeed = 200.0f;
+bool movingright=false;
+
+
+Clock clock;
 while(window.isOpen()){
+Time dt = clock.restart();
+if(!movingright){
+spriteFish.setPosition(spriteFish.getPosition().x-(dt.asSeconds()*fishSpeed),spriteFish.getPosition().y);
+if(spriteFish.getPosition().x<=930){
+movingright=true;
+spriteFish.setScale(-2.5f,2.5f);
+}
+}
+else{
+spriteFish.setPosition(spriteFish.getPosition().x+(dt.asSeconds()*fishSpeed),spriteFish.getPosition().y);
+if(spriteFish.getPosition().x>=2892){
+movingright=false;
+spriteFish.setScale(2.5f,2.5f);
+}
+}
 if(Keyboard::isKeyPressed(Keyboard::Escape)){
 window.close();
 }
@@ -41,6 +72,7 @@ window.close();
 window.clear();
 window.draw(spriteBackground);
 window.draw(spriteFish);
+window.draw(text1);
 window.display();
 }
 }
